f0028e969dd1559adb8ed1492f1c6459fc0ebca3
Serge Satansson <thornonthehead@gmail.com>
2021-03-28 08:30:28 +0300

    Initial

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    public Sprite deadBird;
+    public float horSpeed = 5f;
+    public float verSpeed = 3f;
+    public bool controlEnabled = true;
+    private GameManager gameManager;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        gameManager = FindObjectOfType<GameManager>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!gameManager.gameStarted)
+            return;
+        else
+            anim.SetTrigger("gameStarted");
+
+        if (controlEnabled)
+        {
+            float v = Input.GetAxis("Vertical");
+            rb.velocity = new Vector2(horSpeed, v * verSpeed);
+        }        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb.gravityScale = 10;
+        horSpeed = 0;
+        controlEnabled = false;
+        anim.SetTrigger("dead");
+        GetComponent<BirdController>().enabled = false;
+        GetComponent<Animator>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = deadBird;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameStarted;
+    public void StartGame()
+    {
+        gameStarted = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartGame();
+    }
+}
